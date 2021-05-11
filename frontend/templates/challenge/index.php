05d85e147aed3aefcb4d5d541c0ee873f30095bf
jgladkowski <gladkowskij@gmail.com>
2021-05-11 15:22:46 +0200

    Challenge page v1.0

---
+++ frontend/templates/challenge/index.php
@@ -0,0 +1,178 @@
+<html>
+    <head>
+        <title>Example challenge name</title>
+        <link rel="shortcut icon" href="../../style/img/logo_icon.png">
+        <link rel="stylesheet" type="text/css" href="../../style/css/global-style.css" />
+        <link rel="stylesheet" type="text/css" href="../../style/css/navbar-style.css" />
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script src="script.js"></script>
+        <script src="https://kit.fontawesome.com/67c66657c7.js"></script>
+    </head>
+    <body>
+        <!-- Navbar -->
+        <div id="navbar">
+            <nav>
+                <a href="#">
+                    <div class="logo">
+                        <img src="../../style/img/logo_icon.png" id="logo-icon-normal">
+                        <img src="../../style/img/logo_icon_hover.png" id="logo-icon-hover">
+                        <div id="logo-text">Egzomondo</div>
+                    </div>
+                </a>
+                <ol>
+                    <li><a href="#">My profile</a></li>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                </ol>
+                <div class="search_box">
+                    <input type="search" placeholder="Search">
+                    <a href="#"><span class="fa fa-search"></span></a>
+                </div>
+            </nav>
+        </div>
+        <div id="below-navbar">
+            <div id="container">
+                <!-- Challenge name -->
+                <div id="challenge-name">
+                    <div id="challenge-name-field">
+                        Example challenge name
+                    </div>
+                </div>
+                <!-- Author -->
+                <div id="author-name">
+                    <a href="#">
+                        <div id="author-name-field">
+                            By Janusz
+                        </div>
+                    </a>
+                </div>
+                <!-- Beginning and end -->
+                <div id="time">
+                    <div id="time-field">
+                        11 Sept. 2001 - 11 Sept. 2021
+                    </div>
+                </div>
+                <!-- Goal -->
+                <div id="goal">
+                    <div id="goal-field">
+                        <div id="discipline-img-container">
+                            <img src="../../style/img/disciplines/swimming.png" />
+                        </div>
+                        <div id="goal-field-text">
+                            30 km
+                        </div>
+                    </div>
+                </div>
+                <!-- Users -->
+                <div id="users">
+                    <div id="users-flexbox">
+                        <div class="users-box-elem">
+                            <a href="#">
+                                <div style="width: 50%" class="users-box-elem-progress-bar"></div>
+                                <div class="users-box-elem-link">
+                                    <div class="pfp-container">
+                                        <img src="../../style/img/default-pfp.png" />
+                                    </div>
+                                    <div class="user-name-field">
+                                        miachu
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="users-box-elem">
+                            <a href="#">
+                                <div style="width: 50%" class="users-box-elem-progress-bar"></div>
+                                <div class="users-box-elem-link">
+                                    <div class="pfp-container">
+                                        <img src="../../style/img/default-pfp.png" />
+                                    </div>
+                                    <div class="user-name-field">
+                                        paweu
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="users-box-elem">
+                            <a href="#">
+                                <div style="width: 10%" class="users-box-elem-progress-bar"></div>
+                                <div class="users-box-elem-link">
+                                    <div class="pfp-container">
+                                        <img src="../../style/img/default-pfp.png" />
+                                    </div>
+                                    <div class="user-name-field">
+                                        kamien
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="users-box-elem">
+                            <a href="#">
+                                <div style="width: 80%" class="users-box-elem-progress-bar"></div>
+                                <div class="users-box-elem-link">
+                                    <div class="pfp-container">
+                                        <img src="../../style/img/default-pfp.png" />
+                                    </div>
+                                    <div class="user-name-field">
+                                        kamien
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="users-box-elem">
+                            <a href="#">
+                                <div style="width: 67%" class="users-box-elem-progress-bar"></div>
+                                <div class="users-box-elem-link">
+                                    <div class="pfp-container">
+                                        <img src="../../style/img/default-pfp.png" />
+                                    </div>
+                                    <div class="user-name-field">
+                                        kamien
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="users-box-elem">
+                            <a href="#">
+                                <div style="width: 12%" class="users-box-elem-progress-bar"></div>
+                                <div class="users-box-elem-link">
+                                    <div class="pfp-container">
+                                        <img src="../../style/img/default-pfp.png" />
+                                    </div>
+                                    <div class="user-name-field">
+                                        kamien
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="users-box-elem">
+                            <a href="#">
+                                <div style="width: 45%" class="users-box-elem-progress-bar"></div>
+                                <div class="users-box-elem-link">
+                                    <div class="pfp-container">
+                                        <img src="../../style/img/default-pfp.png" />
+                                    </div>
+                                    <div class="user-name-field">
+                                        kamien
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                        <div class="users-box-elem">
+                            <a href="#">
+                                <div style="width: 20%" class="users-box-elem-progress-bar"></div>
+                                <div class="users-box-elem-link">
+                                    <div class="pfp-container">
+                                        <img src="../../style/img/default-pfp.png" />
+                                    </div>
+                                    <div class="user-name-field">
+                                        kamien
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
