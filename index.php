396aa5fce824c5babfd491ca1591ccb59c5ef33c
Michał Lange <ml418309@students.mimuw.edu.pl>
2021-05-31 19:58:22 +0200

    profilowe przy aktywnosciach

--- index.php
+++ index.php
@@ -110,48 +110,48 @@
             <div class="container">
                 <div class="section-header">Latest activities</div>
                 <div class="container-flex">
-                    <div class="activity">
-                        <div class="activity-user">
-                            <div class="activity-pfp-container">
-                                <img src="img.png" />
-                            </div>
-                            <div class="activity-username">janusz</div>
-                        </div>
-                        <div class="activity-info">
-                            <div class="activity-type-img-container">
-                                <img src="2.png" />
-                            </div>
-                            <div class="">2km / 56min</div>
-                        </div>
-                    </div>
-                    <div class="activity">
-                        <div class="activity-user">
-                            <div class="activity-pfp-container">
-                                <img src="img.png" />
-                            </div>
-                            <div class="activity-username">janusz</div>
-                        </div>
-                        <div class="activity-info">
-                            <div class="activity-type-img-container">
-                                <img src="2.png" />
-                            </div>
-                            <div class="">2km / 56min</div>
-                        </div>
-                    </div>
-                    <div class="activity">
-                        <div class="activity-user">
-                            <div class="activity-pfp-container">
-                                <img src="img.png" />
-                            </div>
-                            <div class="activity-username">janusz</div>
-                        </div>
-                        <div class="activity-info">
-                            <div class="activity-type-img-container">
-                                <img src="2.png" />
-                            </div>
-                            <div class="">2km / 56min</div>
-                        </div>
-                    </div>
+                    <?php
+                        $miesiac_wczesniej = date('Y-m-d');
+                        $miesiac_wczesniej = date("Y-m-d", strtotime("-1 months"));
+                        $stid = oci_parse($conn, "
+                            SELECT K.login, A.id_rodzaju, A.ilosc, A.czas_trwania, A.data_rozpoczecia, K.id
+                            FROM Aktywnosc A, Konto K, Znajomi Z
+                            WHERE ((Z.znajomy1 = K.id AND Z.znajomy2 = ".$_SESSION['id'].") OR (Z.znajomy2 = K.id AND Z.znajomy1 = ".$_SESSION['id']."))
+                            AND A.id = K.id AND A.data_rozpoczecia >= TO_DATE('$miesiac_wczesniej', 'YYYY-MM-DD')
+                            ORDER BY A.data_rozpoczecia DESC
+                        ");
+                        $err = oci_execute($stid);
+
+                        if (!$err) {
+                            echo "Error finding challenge invites:";
+                            $e = oci_error($pars);
+                            var_dump($e);
+                            return;
+                        }
+                        while($row = oci_fetch_array($stid, OCI_BOTH  + OCI_RETURN_NULLS)){
+                            $upload_dir = './frontend/uploads/profilepic/';
+                            echo '
+                            <div class="activity">
+                                <div class="activity-user">
+                                    <div class="activity-pfp-container">';
+                            #echo $upload_dir.$row[5].'.png';
+                            if (file_exists($upload_dir.$row[5].'.png')) {
+                                echo'<img src="'.$upload_dir.$row[5].'.png" />';
+                            } else {
+                                echo        '<img src="./frontend/style/img/default-pfp.png" />';
+                            }
+                            echo   '</div>
+                                    <div class="activity-username">'.$row[0].'</div>
+                                </div>
+                                <div class="activity-info">
+                                    <div class="activity-type-img-container">
+                                        <img src="./frontend/style/img/disciplines/'.$row[1].'.png" />
+                                    </div>
+                                    <div class="">'.$row[2].'km / '.$row[3].'min</div>
+                                </div>
+                            </div>';
+                        }
+                    ?>
                 </div>
             </div>
             <div class="container">
