7a3a2e046663943fe8d67c4f8019f46d73d07427
gabrysiaolsz <gabriela.olszewska11@gmail.com>
2021-05-31 15:23:13 +0200

    Make about page

--- frontend/templates/_common/navbar_not_logged_in.php
+++ frontend/templates/_common/navbar_not_logged_in.php
@@ -4,7 +4,7 @@
             <div class="logo">
                 <img src="../../style/img/logo_icon.png" id="logo-icon-normal">
                 <img src="../../style/img/logo_icon_hover.png" id="logo-icon-hover">
-                <div id="logo-text">Egzomondo - About us</div>
+                <div id="logo-text">Egzomondo - About</div>
             </div>
         </a>
     </nav>
